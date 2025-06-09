32aba8c54ed7ef63c9bac3127a269cc3ca826ff4
kal-i <paul.maceres00@gmail.com>
2025-06-09 23:25:00 +0800

    Added CartRepository and CartItemRepository

---
+++ src/main/java/com/kali/kali_shops/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.kali.kali_shops.repository;
+
+import com.kali.kali_shops.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
