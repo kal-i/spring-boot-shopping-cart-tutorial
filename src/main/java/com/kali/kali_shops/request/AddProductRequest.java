d2fcc23928c22dee51ba83c8e43290b534ceaae9
kal-i <paul.maceres00@gmail.com>
2025-06-17 16:48:59 +0800

    Remove unnecessary id field from AddProductRequest DTO

--- src/main/java/com/kali/kali_shops/request/AddProductRequest.java
+++ src/main/java/com/kali/kali_shops/request/AddProductRequest.java
@@ -5,10 +5,8 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-// We create this class because it isn't available to work directly with entities or model classes
 @Data
 public class AddProductRequest {
-    private Long id;
     private String name;
     private String brand;
     private BigDecimal unitPrice;
